afc46dca79c63302ab1a8f3d4cc9d27c26f3b6e9
akhilcoditas <akhil.sachan@coditas.com>
2023-06-21 17:38:14 +0530

    Box color change states

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,11 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
-  const [currentCount1, setCount1] = useState(0);
-  const [currentCount2, setCount2] = useState(0);
-
   return (
-    <div className="container">
-      <Box
-        counterValue={currentCount1}
-        onIncrement={() => setCount2(currentCount2 + 1)}
-      />
-      <Box
-        counterValue={currentCount2}
-        onIncrement={() => setCount1(currentCount1 + 1)}
-      />
-    </div>
+    <>
+      <Box />
+    </>
   );
-
-  //  WAY 02
-  // const handleOnIncrement = (counterId) => {
-  //   if (counterId === 1) {
-  //     setCount1(currentCount1 + 1);
-  //   } else if (counterId === 2) {
-  //     setCount2(currentCount2 + 1);
-  //   }
-  // }
-  // return (
-  //   <div className="container">
-  //     <Box counterValue={currentCount1} onIncrement={() => handleOnIncrement(1)}/>
-  //     <Box counterValue={currentCount2} onIncrement={() => handleOnIncrement(2)}/>
-  //   </div>
-  // )
 };
 
 export default App;
--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
 import Button from "../Button/Button";
 import styles from "./Box.module.scss";
-import { BoxProps } from "./Box.types";
 
-const Box = ({ counterValue, onIncrement }: BoxProps) => {
+const Box = () => {
+  const [currentColor, setColor] = useState("blue");
+  const [currentButtonColor, setButtonColor] = useState("grey");
 
-  // const [currentCount1, setCount1] = useState(counterValue);
-  // const [currentCount2, setCount2] = useState(counterValue);
-  // const handleOnIncrement = (count) => {
 
-  // }
+  const colors = ["red", "blue", "green", "yellow", "orange", "brown", "black", "skyblue", "royalblue"];
+  
+  const changeColor = () => {
+    const randomColor = Math.floor(Math.random() * colors.length);
+    setButtonColor(currentColor);
+    setColor(colors[randomColor]);
+  };
 
   return (
     <>
       <div>
-        <div className={styles.box}>{counterValue}</div>
-        <Button name={"Increment"} onIncrement={onIncrement}/>
+        <div className={styles.box} style={{backgroundColor: currentColor}}></div>
+        <Button name={"Change Color"} buttonColor={currentButtonColor} onColorChange={changeColor} />
       </div>
     </>
   );
